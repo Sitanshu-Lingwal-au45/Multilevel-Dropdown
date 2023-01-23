83ed4378b5c2e8366bb46c8c8ffee9c9f81cc376
Sitanahu-Lingwal-au45 <slslsingh17@gmail.com>
2023-01-23 09:04:36 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+    return ( <
+        header >
+        <
+        div className = "nav-area" >
+        <
+        a href = "/#"
+        className = "logo" >
+        CodeSolution <
+        /a> <
+        Navbar / >
+        <
+        /div> < /
+        header >
+    );
